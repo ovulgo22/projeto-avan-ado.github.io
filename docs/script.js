8f2add38d7d1c613dc0773f7bab8dc6807b6d247
Matheus M. <matheusmoraesdj2025@gmail.com>
2025-08-24 02:26:05 -0300

    Update and rename script.js to app.js

--- docs/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('myButton').addEventListener('click', function() {
-    const message = document.getElementById('message');
-    message.textContent = 'Você clicou no botão! Aqui estão algumas sugestões para você melhorar suas habilidades!';
-    message.classList.remove('hidden');
-    message.classList.add('show');
-});
